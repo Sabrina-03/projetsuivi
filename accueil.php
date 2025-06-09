3a6cb8a25df76d71dcb27166e675bd68ee9a8a23
Sabrina-03 <zeghoudsabrina24@gmail.com>
2025-06-09 12:28:23 +0200

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -2,7 +2,7 @@
 require_once 'includes/header.php';
 ?>
 
-<!-- Contenu spécifique à la page d'accueil -->
+<!-- Contenu de page d'accueil -->
 <body class="accueil-page">
 
 <h1>Bienvenue dans votre espace de gestion !</h1>
