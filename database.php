f151968f1a56e523a9a03939a3dbb4b9d0521352
Sabrina-03 <zeghoudsabrina24@gmail.com>
2025-06-09 12:34:42 +0200

    Update database.php

--- database.php
+++ database.php
@@ -1,13 +1,19 @@
 <?php
-$host = 'localhost';
-$dbname = 'suivie_équipe'; 
-$username = 'root';
-$password = ''; 
+// Informations pour se connecter à la base de données
+$host = 'localhost';           
+$dbname = 'suivie_équipe';     
+$username = 'root';            
+$password = '';               
+
 try {
+    // Connexion à la base de données avec PDO
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    
+    // Active l'affichage des erreurs si quelque chose ne va pas
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
 } catch (PDOException $e) {
+    // En cas d’erreur de connexion, affiche un message et arrête tout
     die("Erreur de connexion : " . $e->getMessage());
 }
 ?>
